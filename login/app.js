30350c1ef6743bb3c4f3473267e266b725ad031d
Chima Onyekwere <thrashercode@gmail.com>
2016-11-15 00:09:15 -0500

    Update app.js

--- login/app.js
+++ login/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ui.router', 'ngMessages', 'ngStorage', 'ngMockE2E,ui.bootstrap'])
+        .module('app', ['ui.router', 'ngMessages', 'ngStorage', 'ngMockE2E','ui.bootstrap'])
         .config(config)
         .run(run);
 
